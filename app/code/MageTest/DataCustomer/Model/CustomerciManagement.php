28d316b9fa62cd224f679b393b6488414775f029
Jose Valdivia <jose@mail.com>
2023-05-07 14:14:41 -0400

    added method GET REST to get ci by email

---
+++ app/code/MageTest/DataCustomer/Model/CustomerciManagement.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MageTest\DataCustomer\Model;
+use MageTest\DataCustomer\Helper\Data;
+
+class CustomerciManagement implements \MageTest\DataCustomer\Api\CustomerciManagementInterface
+{
+    private $helper;
+
+    public function __construct(
+        Data $helper,
+    ){
+        $this->helper=$helper;
+    }
+
+    public function getCiCustomer($email)
+    {
+        $dataCIUser=$this->helper->getCiCustomer($this->helper->getCustomerIdByEmail($email));
+        return json_encode($dataCIUser,JSON_UNESCAPED_SLASHES);
+    }
+}
